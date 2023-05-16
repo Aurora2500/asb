56dbb3ddd0343c99c0866c2abd4c695afdea4612
Aurora Zuoris <programming.aurora@gmail.com>
2023-05-16 18:30:46 +0100

    Implemented template copying logic

--- src/index.ts
+++ src/index.ts
@@ -6,61 +6,108 @@ import path from "path";
 import chalk from "chalk"
 import inquirer, { DistinctQuestion } from "inquirer";
 import minimist from "minimist";
+import ejs from "ejs";
 
 import { fileURLToPath } from 'url';
+
+type TemplateData = {
+	projectName: string;
+}
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const cwd = process.cwd();
 const choices = fs.readdirSync(path.join(__dirname, "templates"));
-const curr_dir = process.cwd();
 const args = minimist(process.argv.slice(2), {
-	string: ["name", "author", "template"],
+	string: ["name", "template"],
 });
 
-type CLIOption = {
-	projectPath: string;
-	projectName: string;
-	author: string;
-	template: string;
-}
+async function main() {
+	const projectPath: string | undefined = args["_"][0];
+	const fullProjectPath: string = path.resolve(cwd, projectPath || "");
+	const suggestedName = args["name"] != null
+		? args["name"]
+		: path.basename(fullProjectPath);
 
-const projectPath: string | undefined = args["_"][0];
-const fullProjectPath: string = path.resolve(cwd, projectPath || "");
-const suggestedName = args["name"] != null
-	? args["name"]
-	: path.basename(fullProjectPath);
+	const selectedTemplate: string | undefined = args["template"]
 
-const selectedTemplate: string | undefined = args["template"]
+	if (selectedTemplate != undefined && !choices.includes(selectedTemplate)) {
+		console.log(chalk.yellowBright(selectedTemplate), chalk.red("is not a valid template."));
+		console.log(chalk.green("The available templates are:"));
+		choices.forEach(t => {
+			console.log(chalk.blue("*"), chalk.green(t))
+		})
+		process.exit(1);
+	}
 
-if (selectedTemplate != undefined && !choices.includes(selectedTemplate)) {
-	console.log(chalk.yellowBright(selectedTemplate), chalk.red("is not a valid template."));
-	console.log(chalk.green("The available templates are:"));
-	choices.forEach(t => {
-		console.log(chalk.blue("*"), chalk.green(t))
+	console.log({
+		projectPath: fullProjectPath,
+		projectName: suggestedName
 	})
-	process.exit(1);
+
+	const questions: DistinctQuestion[] = []
+
+	if (selectedTemplate == undefined) {
+		questions.push({
+			type: "list",
+			name: "template",
+			message: "Which template do you want to use?",
+			choices
+		})
+	}
+	if (args["name"] == undefined) {
+		questions.push({
+			type: "input",
+			name: "projectName",
+			message: "What project name do you want to use?",
+			default: suggestedName
+		});
+	}
+
+	const answers = await inquirer.prompt(questions);
+
+	const template = selectedTemplate || answers["template"];
+	const projectName = args["name"] || answers["projectName"];
+
+	assertRootDir(fullProjectPath);
+	const templateDir = path.join(__dirname, "templates", template);
+	const templateData: TemplateData = {
+		projectName
+	};
+
+	createProject(fullProjectPath, templateDir, templateData);
 }
 
-console.log({
-	projectPath: fullProjectPath,
-	projectName: suggestedName
-})
-
-const questions: DistinctQuestion[] = []
-
-if (selectedTemplate == undefined) {
-	questions.push({
-		type: "list",
-		name: "template",
-		message: "Which template do you want to use?",
-		choices
-	})
+function assertRootDir(dir: string) {
+	switch (fs.existsSync(dir)) {
+		case true:
+			if (fs.readdirSync(dir).length > 0) {
+				console.log(chalk.red("The root of the project must be empty."));
+				process.exit(1);
+			}
+			break;
+		case false:
+			fs.mkdirSync(dir);
+			break;
+	}
 }
-if (args["name"] == undefined) {
-	questions.push({
-		type: "input",
-		name: "projectName",
-		message: "What project name do you want to use?",
-		default: suggestedName
-	});
+
+function createProject(targetDir: string, templateDir: string, templateData: TemplateData) {
+	// Copy the template files to the target directory recursively
+	const copyRecursive = (src: string, dest: string) => {
+		fs.readdirSync(src).forEach((file) => {
+			const current = path.resolve(src, file);
+			const target = path.resolve(dest, file);
+			if (fs.lstatSync(current).isDirectory()) {
+				fs.mkdirSync(target);
+				copyRecursive(current, target);
+			} else {
+				const content = fs.readFileSync(current, "utf8");
+				const result = ejs.render(content, templateData);
+				fs.writeFileSync(target, result, "utf8");
+			}
+		})
+	}
+	copyRecursive(templateDir, targetDir);
 }
+
+main()
